d4d96e1012e933c6b017602081c6bcbf0fcc0f89
DimitrijeVolarevic <dimitrijevolarevic9@gmail.com>
2023-04-18 08:22:31 +0200

    Initial Commit

---
+++ SwiftUI_Firebase/SwiftUI_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FirebaseApp.swift
+//  SwiftUI_Firebase
+//
+//  Created by Dimitrije Volarevic on 18.4.23..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
